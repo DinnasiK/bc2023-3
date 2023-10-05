9b5125f86122befaa5319705517157916845d8a4
Dmytro <dnechiporuk99@gmail.com>
2023-10-05 12:05:00 +0300

    commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const fs = require("node:fs")
+
+fs.readFile("data.json", (err, data) =>{
+    if(err===null){
+        const d = JSON.parse(data)
+
+        let maxr = 0;
+
+        for(const a of d){
+            if(a.rate > maxr){
+                maxr=a.rate;
+            }
+        }
+
+        fs.writeFile("output.txt", `Максимальний курс: ${maxr}`, (err) => {
+            if(err===null){
+                console.log("The file has been saved!")
+            } else {
+                console.log(err)
+            }
+        }
+        )
+
+    } else {
+        console.log(err)
+    }
+})
